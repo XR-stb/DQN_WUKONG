77351aa5e9c34186b203e86e998c2d389203ba5e
flufy3d <flufy3d@gmail.com>
2024-09-14 10:35:49 +0200

    add yinhu restart func

--- restart.py
+++ restart.py
@@ -100,8 +100,47 @@ def huxianfeng_restart_v2():
     actions.run_with_dircet(1, 'W')
 
 
+
+def yinhu_restart():
+    log("已死亡,自动寻路训练开始")
+
+    # 等复活到土地庙先
+    wait = 20
+    for i in range(wait, 0, -1):
+        keys = key_check()
+        if 'B' in keys:
+            return
+        log('%d秒后复活' % i)
+        actions.precise_sleep(1)
+
+    actions.go_forward(1.5)
+    actions.precise_sleep(1) # 必须等一会儿，不然按键会没响应
+
+    actions.press_E()
+    actions.precise_sleep(5)
+
+    # 菜单会停留在上次选的那个项
+    # actions.press_down()
+    # actions.precise_sleep(1)
+    # actions.press_down()
+    # actions.precise_sleep(1)
+
+    actions.dodge()
+    actions.precise_sleep(5)
+
+    log('长按e 跳过动画！')
+    actions.press_E(3)
+    actions.precise_sleep(8)
+    
+    log('ready!')
+    #开了自动锁定了
+    #actions.lock_view()
+    actions.run_with_dircet(1, 'W')
+    actions.pause()
+
+
 def restart():
-    huxianfeng_restart_v1()
+    yinhu_restart()
 
 if __name__ == "__main__":  
     restart()
